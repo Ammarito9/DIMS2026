8c0dc262be708c547b3811ad910712f10809fb73
Ammar Al-Baghajati <AmmarAlBaghajati@gmail.com>
2026-02-19 16:15:35 +0300

    Impleminting supervises

--- src/main/java/org/example/demo2/Controller/SupervisesController.java
+++ src/main/java/org/example/demo2/Controller/SupervisesController.java
@@ -1,4 +1,73 @@
 package org.example.demo2.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class SupervisesController {
+
+    @FXML
+    private Button btn_Clear;
+
+    @FXML
+    private Button btn_Close;
+
+    @FXML
+    private Button btn_Delete;
+
+    @FXML
+    private Button btn_Get;
+
+    @FXML
+    private Button btn_Save;
+
+    @FXML
+    private Button btn_Update;
+
+    @FXML
+    private Label lbl_Performance;
+
+    @FXML
+    private Label lbl_Student;
+
+    @FXML
+    private Spinner<Integer> spinner_Performance;
+
+    @FXML
+    private TextField tf_Student;
+
+    @FXML
+    void Clear_Button_On_Action(ActionEvent event) {
+        tf_Student.clear();
+        spinner_Performance.cancelEdit();
+    }
+
+    @FXML
+    void Close_Button_On_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Delete_Button_On_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Get_Button_On_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Save_Button_On_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Update_Button_On_Action(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/org/example/demo2/dto/Supervises.java
+++ src/main/java/org/example/demo2/dto/Supervises.java
@@ -1,4 +1,30 @@
 package org.example.demo2.dto;
 
 public class Supervises {
+    private String Student;
+    private int Performance ;
+
+    public Supervises()
+    {
+
+    }
+
+    public Supervises(String Student, int Performance)
+    {
+        this.Student = Student;
+        this.Performance = Performance;
+    }
+
+    public String GetStudent() {
+        return Student;
+    }
+    public void SetStudent(String Student) {
+        this.Student = Student;
+    }
+    public int GetPerformance() {
+        return Performance;
+    }
+    public void SetPerformance(int Performance) {
+        this.Performance = Performance;
+    }
 }
