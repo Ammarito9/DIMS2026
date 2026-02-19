a6238cabd66ee48fd54288f2d85644d5376dd757
ZiyadAlboghdady <ziyadalboghdady@gmail.com>
2026-02-19 17:52:15 +0300

    adding get functions and applying it to the btn

--- src/main/java/org/example/demo2/Controller/StudiesController.java
+++ src/main/java/org/example/demo2/Controller/StudiesController.java
@@ -1,9 +1,15 @@
 package org.example.demo2.Controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.example.demo2.db.StudiesCrudOperations;
+import org.example.demo2.dto.Studies;
+
+import java.util.Optional;
 
 public class StudiesController {
 
@@ -36,21 +42,41 @@ public class StudiesController {
 
     @FXML
     void Clear_Btn_On_Action(ActionEvent event) {
+        tf_id.clear();
+        tf_description.clear();
+        tf_title.clear();
 
     }
 
     @FXML
     void Close_Btn_On_Action(ActionEvent event) {
-
+        Platform.exit();
     }
 
     @FXML
     void Delete_Btn_On_Action(ActionEvent event) {
-
+        checkId(tf_id.getText(), event);
+        StudiesCrudOperations crudOperations=new StudiesCrudOperations();
+        int id = Integer.parseInt(tf_id.getText());
+//continue the delete part and make a function in the studies crud operations file and then make the rest of the functions
     }
 
     @FXML
     void Get_Btn_On_Action(ActionEvent event) {
+        checkId(tf_id.getText(), event);
+        StudiesCrudOperations crudOperations=new StudiesCrudOperations();
+        int id = Integer.parseInt(tf_id.getText());
+        Optional<Studies> studies=crudOperations.getStudiesById(id);
+        if(studies.isPresent()){
+            tf_id.setText(Integer.toString(studies.get().getId()));
+            tf_description.setText(studies.get().getDescription());
+            tf_title.setText(studies.get().getTitle());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Car with id " + id + " not found");
+            alert.showAndWait();
+        }
 
     }
 
@@ -62,6 +88,15 @@ public class StudiesController {
     @FXML
     void Update_Btn_On_Action(ActionEvent event) {
 
+    }
+    public void checkId(String id, ActionEvent event) {
+        if (id.isEmpty() || Integer.parseInt(id) <= 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Id is wrong!");
+            alert.showAndWait();
+            Clear_Btn_On_Action(event);
+        }
     }
 
 }
--- src/main/java/org/example/demo2/db/StudiesCrudOperations.java
+++ src/main/java/org/example/demo2/db/StudiesCrudOperations.java
@@ -1,8 +1,34 @@
 package org.example.demo2.db;
 
+import org.example.demo2.dto.Studies;
+
+import java.sql.*;
+import java.util.Optional;
+
 public class StudiesCrudOperations {
     static final String DB_URL ="jdbc:postgresql://localhost:5432/DIMS1";
     static final String USER ="postgres";
     static final String PASS="0000";
 
+    public Optional<Studies> getStudiesById(int id){
+        Studies studies= null;
+        try(Connection connection= DriverManager.getConnection(DB_URL,USER,PASS)){
+            Statement statement = connection.createStatement();
+            String query="SELECT * FROM studies WHERE id = "+id;
+            ResultSet resultSet=statement.executeQuery(query);
+            while(resultSet.next()){
+                studies = new Studies();
+                studies.setId(resultSet.getInt("id"));
+                studies.setDescription(resultSet.getString("description"));
+                studies.setTitle(resultSet.getString("title"));
+            }
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        if (studies!=null)
+            return Optional.of(studies);
+        else
+            return Optional.empty();
+    }
 }
